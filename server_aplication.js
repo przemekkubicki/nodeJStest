fb3f3d5e71e0561da82e36d74090b81ca2d4c8d3
Przemyslaw Kubicki <pkubickii@aol.com>
2015-02-01 19:52:04 +0100

    initial commit

---
+++ server_aplication.js
@@ -0,0 +1,61 @@
+var soap = require('soap-server');
+var fs = require("fs");
+var gm = require('gm');
+var easyimg = require('easyimage');
+
+var Data;
+var serverInstance = new soap.SoapServer();
+
+var imgSizeMob = 320;
+var imgSizeOrg = 1000;
+var image = "mapa.jpg";
+var out_image = "out.jpg";
+
+
+fs.readFile(image, function(err, original_data){
+	if (err) {
+		throw err;
+	}
+    Data= new Buffer(original_data, 'binary').toString('base64');
+});
+
+/**
+ *
+ * @constructor
+ */
+function Service(){};
+Service.customCb = function(d) {};
+Service.prototype.getmapfragment = function(sx, sy, ex, ey){
+    var that = this;
+    if (sx == 0 && sy == 0 && ex == 0 && ey == 0)
+        that.customCb(Data);
+    else
+        __loadImage(sx, sy, ex, ey, function(data) { that.customCb(data); });
+};
+
+soapService = serverInstance.addService('getmapfragment', new Service());
+serverInstance.listen(777, '127.0.0.1');
+
+
+/**
+ * This loads and image form the mobile resolution to org res and
+ * get back the result
+ 
+ */
+function __loadImage(sx, sy, ex, ey, cb) {
+    var _x = (sx/imgSizeMob)*imgSizeOrg;
+    var _y = (sy/imgSizeMob)*imgSizeOrg;
+    var width = (ex/imgSizeMob)*imgSizeOrg;
+    var height = (ey/imgSizeMob)*imgSizeOrg;
+    easyimg.crop(
+        {
+            src:image, dst:out_image,
+            cropwidth: width, cropheight:height,
+            gravity:'NorthWest',
+            x:_x, y:_y
+        }).then(function(){
+            fs.readFile(out_image, function(err, original_data){
+                cb(new Buffer(original_data, 'binary').toString('base64'));
+            });
+        });
+};
